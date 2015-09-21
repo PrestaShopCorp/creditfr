04333fcb602596a25e8e2177ffaebcc6da534b08
Thomas Nabord <thomas.nabord@prestashop.com>
2015-09-21 19:00:44 +0200

    Update version (v1.0.1)

--- creditfr.php
+++ creditfr.php
@@ -35,7 +35,7 @@ class Creditfr extends Module
 	{
 		$this->name = 'creditfr';
 		$this->tab = 'others';
-		$this->version = '1.0.0';
+		$this->version = '1.0.1';
 		$this->author = 'PrestaShop';
 
 		$this->bootstrap = true;
